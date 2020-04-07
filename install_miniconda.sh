0168ed8aebfedfe4df691128a2f5d8d0ae059b40
Matthew Dillon <matthewrdillon@gmail.com>
2020-04-07 15:29:42 -0700

    BUG: hmm

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -21,7 +21,7 @@ chmod +x miniconda.sh
 ./miniconda.sh -b -p $1
 
 conda init
-exec "$BASH"
+exec bash
 
 conda upgrade -n base -q -y -c defaults --override-channels conda
 conda install -n base -q -y -c defaults --override-channels conda-build conda-verify
