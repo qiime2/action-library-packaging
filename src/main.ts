7c984530fa5a176f34b9db278701c3605942beb2
Anthony <oddant1@hotmail.com>
2020-06-11 09:32:11 -0700

    SQUASH: Remove doc code for now

--- src/main.ts
+++ src/main.ts
@@ -41,24 +41,12 @@ async function main(): Promise<void> {
       throw Error('miniconda install failed')
     }
 
-    let myOutput = '';
-    let myError = '';
-
-    const options = {
-      stdout: (data: Buffer) => {
-        myOutput += data.toString();
-      },
-      stderr: (data: Buffer) => {
-        myError += data.toString();
-      }
-    };
-
     const recipePath: string = core.getInput('recipe-path')
     const buildPackScriptExitCode = await exec.exec('conda', ['build', '-c', 'qiime2-staging/label/r2020.6',
                                                               '-c', 'conda-forge', '-c', 'bioconda',
                                                               '-c', 'defaults', '--override-channels',
                                                               '--output-folder', buildDir,
-                                                              '--no-anaconda-upload', recipePath], options)
+                                                              '--no-anaconda-upload', recipePath])
     if (buildPackScriptExitCode !== 0) {
       throw Error('package building failed')
     }
