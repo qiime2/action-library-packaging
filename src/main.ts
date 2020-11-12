30967fc8462fc27940bbca696405eec7b88325c2
Matthew Dillon <matthewrdillon@gmail.com>
2020-11-12 11:58:36 -0700

    testing

--- src/main.ts
+++ src/main.ts
@@ -67,10 +67,10 @@ async function installMiniconda(homeDir: string | undefined, condaURL: string) {
 
     core.addPath(minicondaBinDir)
 
-    await execWrapper('wget', ['-O', 'miniconda.sh', condaURL])
-    await execWrapper('chmod', ['+x', 'miniconda.sh'])
+    // await execWrapper('wget', ['-O', 'miniconda.sh', condaURL])
+    // await execWrapper('chmod', ['+x', 'miniconda.sh'])
 
-    await execWrapper('./miniconda.sh', ['-b', '-p', minicondaDir])
+    // await execWrapper('./miniconda.sh', ['-b', '-p', minicondaDir])
 
     await execWrapper('conda', ['upgrade', '-n', 'base', '-q', '-y', '-c', 'defaults', '--override-channels', 'conda'])
 }
