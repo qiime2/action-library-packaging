47e720bb915d57e1de12e558804e2f218cd2376f
Liz Gehret <elizabeth.gehret@gmail.com>
2021-05-20 15:03:49 -0700

    await.js

--- bin/artifact-upload/script.js
+++ bin/artifact-upload/script.js
@@ -2,12 +2,16 @@ const artifact = require('@actions/artifact');
 const glob = require('@actions/glob');
 const core = require('@actions/core');
 
-try {
-    const filesGlobber = await glob.create(`${process.env.BUILD_DIR}/*/**`);
-    const files = await filesGlobber.glob();
+async function artifactUpload() {
+    try {
+        const filesGlobber = await glob.create(`${process.env.BUILD_DIR}/*/**`);
+        const files = await filesGlobber.glob();
 
-    const artifactClient = artifact.create();
-    artifactClient.uploadArtifact(process.env.ARTIFACT_NAME, files, process.env.BUILD_DIR);
-} catch (error) {
-    core.setFailed(error.message);
+        const artifactClient = artifact.create();
+        artifactClient.uploadArtifact(process.env.ARTIFACT_NAME, files, process.env.BUILD_DIR);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+artifactUpload();
