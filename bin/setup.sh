2b059ae995525c8970ab17dadfe66d2b1b202315
Liz Gehret <elizabeth.gehret@gmail.com>
2021-05-20 10:09:26 -0700

    syntax updates

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,12 +8,12 @@ REL_CYCLE=2021.4
 case "$RUNNER_OS" in
     macOS)
         echo "ARTIFACT_NAME=osx-64" >> $GITHUB_ENV
-        PLATFORM='osx'
+        PLATFORM="osx"
         ;;
 
     Linux)
         echo "ARTIFACT_NAME=linux-64" >> $GITHUB_ENV
-        PLATFORM='linux'
+        PLATFORM="linux"
         ;;
 
     *)
@@ -23,23 +23,23 @@ esac
 
 case "$BUILD_TARGET" in
     tested)
-        Q2_CHANNEL='https://packages.qiime2.org/qiime2/${DEV_CYCLE}/tested'
-        ENV_URL='https://raw.githubusercontent.com/qiime2/environment-files/master/${DEV_CYCLE}/test/qiime2-${DEV_CYCLE}-py38-${PLATFORM}-conda.yml'
+        Q2_CHANNEL="https://packages.qiime2.org/qiime2/${DEV_CYCLE}/tested"
+        ENV_URL="https://raw.githubusercontent.com/qiime2/environment-files/master/${DEV_CYCLE}/test/qiime2-${DEV_CYCLE}-py38-${PLATFORM}-conda.yml"
         ;;
 
     staged)
-        Q2_CHANNEL='https://packages.qiime2.org/qiime2/${DEV_CYCLE}/staged'
-        ENV_URL='https://raw.githubusercontent.com/qiime2/environment-files/master/${DEV_CYCLE}/staging/qiime2-${DEV_CYCLE}-py38-${PLATFORM}-conda.yml'
+        Q2_CHANNEL="https://packages.qiime2.org/qiime2/${DEV_CYCLE}/staged"
+        ENV_URL="https://raw.githubusercontent.com/qiime2/environment-files/master/${DEV_CYCLE}/staging/qiime2-${DEV_CYCLE}-py38-${PLATFORM}-conda.yml"
         ;;
 
     released)
-        Q2_CHANNEL='qiime2/label/r${REL_CYCLE}'
-        ENV_URL='https://raw.githubusercontent.com/qiime2/environment-files/master/${REL_CYCLE}/release/qiime2-${REL_CYCLE}-py38-${PLATFORM}-conda.yml'
+        Q2_CHANNEL="qiime2/label/r${REL_CYCLE}"
+        ENV_URL="https://raw.githubusercontent.com/qiime2/environment-files/master/${REL_CYCLE}/release/qiime2-${REL_CYCLE}-py38-${PLATFORM}-conda.yml"
         ;;
 
     *)
-        Q2_CHANNEL='qiime2/label/r${REL_CYCLE}'
-        ENV_URL='https://raw.githubusercontent.com/qiime2/environment-files/master/${REL_CYCLE}/release/qiime2-${REL_CYCLE}-py38-${PLATFORM}-conda.yml'
+        Q2_CHANNEL="qiime2/label/r${REL_CYCLE}"
+        ENV_URL="https://raw.githubusercontent.com/qiime2/environment-files/master/${REL_CYCLE}/release/qiime2-${REL_CYCLE}-py38-${PLATFORM}-conda.yml"
         ;;
 esac
 echo "Q2_CHANNEL=$Q2_CHANNEL" >> $GITHUB_ENV
