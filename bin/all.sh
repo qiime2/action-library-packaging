27b3be8141bd003d89cd55dbbc966cf72b6f30bd
Liz Gehret <elizabeth.gehret@gmail.com>
2021-05-21 12:56:02 -0700

    ...

--- bin/all.sh
+++ bin/all.sh
@@ -4,8 +4,8 @@ set -e
 
 # Update when changing any subsequent bin/ scripts
 # action steps
-bash setup.sh
-bash build.sh
+bash bin/setup.sh
+bash bin/build.sh
 node --unhandled-rejections=strict artifact-upload/script.js
-bash testing.sh
-bash library.sh
+bash bin/testing.sh
+bash bin/library.sh
