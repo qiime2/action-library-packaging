274ee7372579056a9e2c794ca8c2d833144d99e9
Matthew Dillon <matthewrdillon@gmail.com>
2021-05-24 16:38:49 -0700

    trying another approach

--- main/script.js
+++ main/script.js
@@ -4,11 +4,10 @@ const path = require('path');
 
 async function main() {
     try {
-        await exec.exec('bash alp-7a862f3805c8-all.sh');
+        await exec.exec(`bash ${path.resolve(__dirname, '..', 'bin', 'all.sh')}`);
     } catch (error) {
         core.setFailed(error.message);
     }
 }
 
-core.addPath(`${path.resolve(__dirname, '..')}/bin`);
 main().catch(core.setFailed);
