f9c7d2a0f779a7adc987435cd693407bf0221db2
Matthew Dillon <matthewrdillon@gmail.com>
2021-07-09 16:39:06 -0700

    isolation

--- bin/alp-1c8c84a3-build.sh
+++ bin/alp-1c8c84a3-build.sh
@@ -27,5 +27,6 @@ sudo conda build \
     --override-channels \
     --output-folder $BUILD_DIR \
     --no-anaconda-upload \
-    --append-file $additional_tests_fp \
     $RECIPE_PATH
+
+# --append-file $additional_tests_fp \
