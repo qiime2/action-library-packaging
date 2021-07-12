e80a1cef935585adf5067f4cf17f0464f91b2cf0
Liz Gehret <elizabeth.gehret@gmail.com>
2021-07-11 20:53:01 -0700

    refactoring into a few smaller functions

--- drafting/conda-template.py
+++ drafting/conda-template.py
@@ -10,31 +10,43 @@ import os
 import sys
 from jinja2 import Template
 
-if __name__ == '__main__':
-    # Reading the qiime2 recipe file
+def validate_params(qiime2_recipe, filepath):
     try:
-        qiime2_recipe = sys.argv[1]
+        qiime2_recipe
     except:
         raise ValueError('Missing required parameter. QIIME 2 recipe filepath must be provided.')
-    
-    with open(qiime2_recipe) as fh:
-        parsed_recipe = yaml.load(fh, Loader=yaml.FullLoader)
 
     try:
-        filepath = sys.argv[2]
+        filepath
     except:
         raise ValueError('Missing required parameter. conda recipe filename must be provided.')
     
     if os.path.exists(filepath):
         raise FileExistsError('Invalid filename. Recipe file already exists.')
 
-    # Accessing jinja template with recipe key pairs
-    with open('jinja-template.j2') as fh:
+    return qiime2_recipe, filepath
+
+def _yaml_parsing(qiime2_recipe):
+    with open(qiime2_recipe) as fh:
+        parsed_recipe = yaml.load(fh, Loader=yaml.FullLoader)
+
+    return parsed_recipe
+
+def write_recipe(j2, filepath):
+    with open(j2) as fh:
         template = Template(fh.read())
 
-    recipe_reqs = template.render(parsed_recipe)
+    recipe_reqs = template.render(_yaml_parsing(qiime2_recipe))
 
     with open(filepath, 'w') as fh:
         fh.write(recipe_reqs)
+
+if __name__ == '__main__':   
+    qiime2_recipe = sys.argv[1]
+    filepath = sys.argv[2]
+    j2 = 'jinja-template.j2'
+
+    validate_params(qiime2_recipe, filepath)
+    write_recipe(j2, filepath)
 
 #TODO: add ABOUT segment from plugin into jinja template
