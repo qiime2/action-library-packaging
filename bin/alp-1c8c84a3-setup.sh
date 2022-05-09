cbcdd4b7a458d908a6caababfc713280fe8eb6cc
Matthew Dillon <matthewrdillon@gmail.com>
2022-05-09 15:37:29 -0700

    imp: use mamba solver for builds (#39)

--- bin/alp-1c8c84a3-setup.sh
+++ bin/alp-1c8c84a3-setup.sh
@@ -2,6 +2,21 @@
 
 set -e
 
-sudo conda upgrade -n base -q -y -c defaults --override-channels conda
+sudo conda upgrade \
+  -n base \
+  -q \
+  -y \
+  -c defaults \
+  --override-channels \
+  conda
 
-sudo conda install -n base -q -y -c defaults --override-channels conda-build conda-verify
+sudo conda install \
+  -n base \
+  -q \
+  -y \
+  -c conda-forge \
+  -c defaults \
+  --override-channels \
+  boa \
+  conda-build \
+  conda-verify
