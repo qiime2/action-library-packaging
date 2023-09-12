c4a98799cd722c1e303d01d8bd0d3fc4f32697f6
Evan Bolyen <ebolyen@gmail.com>
2023-09-12 10:04:13 -0700

    Use mamba

--- install-package/bin/install_package.py
+++ install-package/bin/install_package.py
@@ -12,7 +12,7 @@ def main(channels, package_name, package_version, conda_prefix):
         [('-c', channel) for channel in channels])
 
     cmd = [
-        'conda', 'install',
+        'mamba', 'install',
         '-p', conda_prefix,
         f'{package_name}={package_version}',
         *channels, '--override-channels',
