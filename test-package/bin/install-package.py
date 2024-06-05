2f65b70a685a53984d82f5e5774a1f41ad15b4db
Oddant1 <oddant1@hotmail.com>
2024-06-05 11:28:24 -0700

    Split testing into two stages

--- test-package/bin/install-package.py
+++ test-package/bin/install-package.py
@@ -1,6 +1,7 @@
+
 #!/usr/bin/env python
 
-import importlib
+import itertools
 import subprocess
 import tarfile
 import yaml
@@ -15,25 +16,22 @@ def find_tests(package_path):
     return tests
 
 
-def run_imports(imports):
-    for imp in imports:
-        print(f'Importing: {imp}', flush=True)
-        importlib.import_module(imp)
-
-
-def run_commands(commands):
-    for cmd in commands:
-        print(f'Running: {cmd}', flush=True)
-        subprocess.run(cmd, shell=True, check=True)
+def install_requires(reqs, channels):
+    print(f'Installing: {" ".join(reqs)}', flush=True)
+    channels = itertools.chain.from_iterable(
+        [('-c', channel) for channel in channels])
+    subprocess.run(['conda', 'install',
+                    *channels,
+                    '-y', '-q',
+                    *reqs],
+                   check=True)
 
 
 def main(package_path, channels, conda_activate):
     tests = find_tests(package_path)
     print(tests, flush=True)
-    if 'imports' in tests:
-        run_imports(tests['imports'])
-    if 'commands' in tests:
-        run_commands(tests['commands'])
+    if 'requires' in tests:
+        install_requires(tests['requires'], channels)
 
 
 if __name__ == '__main__':
