f644de58fd99299120de7599bc47091b07ccd510
Liz Gehret <elizabeth.gehret@gmail.com>
2024-08-12 15:12:58 -0700

    unbreak things

--- build-package/bin/build-package.py
+++ build-package/bin/build-package.py
@@ -40,8 +40,8 @@ def get_pkg_name_and_version(recipe_path):
     pkg_name = result_json['install'][0]['metadata']['name']
     pkg_version = result_json['install'][0]['metadata']['version']
 
-    subprocess.run(['ls', pip_install_path], check=True)
-    raise ValueError(pip_install_path)
+    subprocess.run(f'echo "{result_json}" > ./report.json',
+                   shell=True, check=True)
 
     return pkg_name, pkg_version
 
