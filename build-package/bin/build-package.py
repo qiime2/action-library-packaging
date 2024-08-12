58b4f1e07c59eb27353d7515f5c83ebe3ceca442
Liz Gehret <elizabeth.gehret@gmail.com>
2024-08-12 15:50:20 -0700

    where is this one more time

--- build-package/bin/build-package.py
+++ build-package/bin/build-package.py
@@ -40,6 +40,8 @@ def get_pkg_name_and_version(recipe_path):
     pkg_name = result_json['install'][0]['metadata']['name']
     pkg_version = result_json['install'][0]['metadata']['version']
 
+    raise ValueError(pip_install_path)
+
     return pkg_name, pkg_version
 
 
