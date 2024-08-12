357cadb86b610fa14478e1eff02eb92a5afaa527
Liz Gehret <elizabeth.gehret@gmail.com>
2024-08-12 15:37:26 -0700

    let's try this approach instead

--- build-package/bin/build-package.py
+++ build-package/bin/build-package.py
@@ -32,17 +32,13 @@ def get_pkg_name_and_version(recipe_path):
     ]
 
     subprocess.run(cmd, check=True, capture_output=True)
-    result = subprocess.run(['cat', 'report.json'], check=True,
-                            capture_output=True, text=True)
+    result = subprocess.run(['cat', 'report.json', '>', './report.json'],
+                            check=True, capture_output=True, text=True)
 
     result_json = json.loads(result.stdout)
-    json_fp = './report.json'
 
     pkg_name = result_json['install'][0]['metadata']['name']
     pkg_version = result_json['install'][0]['metadata']['version']
-
-    subprocess.run([f'echo """{result_json}""" > {json_fp}'],
-                   shell=True, check=True)
 
     return pkg_name, pkg_version
 
