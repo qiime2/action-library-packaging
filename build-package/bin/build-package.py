e9214a66a5434bf60a884ad89094cbf0924142fc
Liz Gehret <elizabeth.gehret@gmail.com>
2024-08-22 16:16:56 -0700

    remove print, we know things are good on this end

--- build-package/bin/build-package.py
+++ build-package/bin/build-package.py
@@ -85,7 +85,6 @@ def main(recipe_path, conda_build_config, channels,
         name, version = get_pkg_name_and_version(recipe_path)
         env = os.environ.copy()
         env['PKG_VERSION'] = version
-        print(f'PKG_VERSION: {env["PKG_VERSION"]}')
 
     if not dry_run:
         print(f'Running: {" ".join(cmd)}', flush=True)
