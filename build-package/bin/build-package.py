7627d63fc10dda363642aad268a5683e4e92def0
Liz Gehret <elizabeth.gehret@gmail.com>
2024-12-17 10:10:15 -0700

    let's just see what this is

--- build-package/bin/build-package.py
+++ build-package/bin/build-package.py
@@ -23,6 +23,8 @@ def get_pkg_name_and_version(recipe_path):
     if not os.path.isdir(recipe_path):
         raise Exception(f'{recipe_path} is not a directory')
 
+    raise ValueError(recipe_path)
+
     if recipe_path == 'ci/recipe':
         pip_install_path = os.path.join(recipe_path, '..', '..')
     else:
