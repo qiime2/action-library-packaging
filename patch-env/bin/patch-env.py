f1f8f09fc2dfb0b6913c9b894f6e479d446945dc
Liz Gehret <54517601+lizgehret@users.noreply.github.com>
2025-01-09 16:30:01 -0700

    debug

--- patch-env/bin/patch-env.py
+++ patch-env/bin/patch-env.py
@@ -53,6 +53,8 @@ def main(conda_activate, environment_file, versions_file,
         if pkg in package_order:
             idx = package_order[pkg]
             version = package_versions[pkg]
+            print(new_version)
+            raise ValueError(version)
             # We want to use the new version in the case of a partial order
             if not parse(new_version) < parse(version):
                 deps[idx] = spec
