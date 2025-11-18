29fce3dca1c1aeaf39d891a105abf533ad014def
Liz Gehret <elizabeth.gehret@gmail.com>
2025-11-18 14:44:23 -0700

    BUG: attempt to fix glibc issue w/older linux runners

--- collect-package/bin/collect_package.py
+++ collect-package/bin/collect_package.py
@@ -6,7 +6,7 @@ from alp.common import ActionAdapter
 
 
 def _os_detector(runner):
-    if runner == 'ubuntu-latest':
+    if runner == 'ubuntu-22.04':
         subdir = 'linux-64'
     elif runner == 'macos-15-intel':
         subdir = 'osx-64'
